4bb872b10d6ebcda2b72a2e0c24daa2130d842b8
MaximusMiloi <Maximusmiloi@gmail.com>
2018-09-14 01:53:25 +0300

    1 Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+
+    temperature = (temperature *9/5)+32;
+    return temperature;
+    warmup(-20);
+    warmup(0);
+    warmup(15);
+    warmup(40);
+
 };
